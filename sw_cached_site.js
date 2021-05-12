ee39e78a9dce430f4059205a04439b472ce64f45
Mohit Kumar <mohitk0208@users.noreply.github.com>
2021-05-12 21:30:28 +0530

    bug fix

--- sw_cached_site.js
+++ sw_cached_site.js
@@ -11,7 +11,7 @@ const appShellfiles = [
     "/notes-pwa/manifest.webmanifest"
 ]
 
-self.addEventListener("install", () => {
+self.addEventListener("install", (e) => {
     console.log("Service Worker: Installed");
     
     e.waitUntil((async () => {
