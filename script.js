eb96d60a155913242c94e9a67e028bbc6239a3ec
Mohit Kumar <mohitk0208@users.noreply.github.com>
2021-05-16 14:32:56 +0530

    made function to create the file list in left panel

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const body = document.querySelector("body");
 const indicator = document.getElementById("save-indicator");
 const notepad = document.getElementById("notepad");
 const fileNameElement = document.getElementById("filename");
-const addFileBtn = document.querySelector(".add-btn")
+const addFileBtn = document.querySelector(".add-btn");
+const filesContainer = document.querySelector(".files-container");
 
 // if (navigator.serviceWorker) {
 // 	window.addEventListener("load", () => {
@@ -18,18 +19,14 @@ const addFileBtn = document.querySelector(".add-btn")
 // 	});
 // }
 
-
-
 // let note = new Note(fileNameElement.innerText, notepad.value);
 let note = null;
 let timeOut = null;
 
-
 const SAVE_STATUS = {
-	SAVING:"hsl(39, 100%, 50%)",
-	SAVED:"hsl(120, 100%, 25%)"
-}
-
+	SAVING: "hsl(39, 100%, 50%)",
+	SAVED: "hsl(120, 100%, 25%)",
+};
 
 // set the mode according to the system preference
 SetDarkModeAndAddEventListener();
@@ -50,53 +47,94 @@ if (currentFileId) {
 	createNewFileAndOpen();
 }
 
+// initialize the file list in left panel
+function updateFileList() {
+	getAllNotes().then((notes) => {
+		const content = notes
+			.reverse()
+			.map((n) => {
+				return createHTMLFile(n);
+			})
+			
+			content.forEach(c => {
+				filesContainer.appendChild(c)
+			})
+
+		console.log(content);
+
+		// filesContainer.innerHTML = "";
+		// filesContainer.insertAdjacentHTML("afterbegin", content);
+	});
+}
+
+updateFileList();
+
 (async () => {
 	let x = await getAllNotes();
 	console.log(x);
 })();
 
-
 // ALL EVENT LISTENERS HERE....
 
-addFileBtn.addEventListener("click",(e) => {
-	createNewFileAndOpen()
-})
+addFileBtn.addEventListener("click", (e) => {
+	createNewFileAndOpen();
+	updateFileList();
+});
 
 fileNameElement.addEventListener("input", function (e) {
 	note.name = fileNameElement.innerText;
 
-	setIndicatorStatusColor(SAVE_STATUS.SAVING)
+	setIndicatorStatusColor(SAVE_STATUS.SAVING);
 	clearTimeoutIfExistAndCallSaveFunctionWithTimeout();
 });
 
 notepad.addEventListener("input", () => {
 	note.content = notepad.value;
 
-	setIndicatorStatusColor(SAVE_STATUS.SAVING)
+	setIndicatorStatusColor(SAVE_STATUS.SAVING);
 	clearTimeoutIfExistAndCallSaveFunctionWithTimeout();
 });
-
-
-
-
-
-
-
 
 function save() {
 	addOrUpdateNote(note)
 		.then(() => {
 			timeOut = null;
-			setIndicatorStatusColor(SAVE_STATUS.SAVED)
+			setIndicatorStatusColor(SAVE_STATUS.SAVED);
 		})
 		.catch((err) => {
 			console.log(err);
 		});
 }
 
-function createNewFileAndOpen() {
+function createHTMLFile(note) {
+	const content = `
+		<div class="file">
+			<p>${note.name}</p>
+			<button class="delete-btn remove-btn-style" data-note-id=${note.id} >
+				DEL
+			</button>
+		</div>
+		`;
 
-	setIndicatorStatusColor(SAVE_STATUS.SAVING)
+	const div = document.createElement("div")
+	div.classList.add("file")
+
+	const p = document.createElement("p")
+	p.innerText = note.name
+
+	const button = document.createElement("button")
+	button.innerText = "DEL"
+	button.classList.add(...["delete-btn","remove-btn-style"])
+	button.setAttribute("data-note-id",note.id)
+
+	div.appendChild(p)
+	div.appendChild(button)
+
+	return div;
+}
+
+function createNewFileAndOpen() {
+	setIndicatorStatusColor(SAVE_STATUS.SAVING);
 	openNoteInEditor(new Note("something", ""));
 	save();
 }
@@ -134,9 +172,8 @@ function SetDarkModeAndAddEventListener() {
 }
 
 function setIndicatorStatusColor(color) {
-	indicator.style.backgroundColor = color
+	indicator.style.backgroundColor = color;
 }
-
 
 function clearTimeoutIfExistAndCallSaveFunctionWithTimeout() {
 	if (timeOut) clearTimeout(timeOut);
